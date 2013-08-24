a1401ed94e1d5624599e7c0a99576c853d58870f
Roy Crihfield <rscrihf@gmail.com>
2013-08-24 16:26:01 -0500

    cleanup

--- component.hh
+++ component.hh
@@ -16,24 +16,20 @@ struct Component
 
 /** The Component trait, or contract.
  *      . must inherit from Component
- *      . must have a ::Facade struct
+ *      X must have a ::Facade struct // instead, select those needing one?
  *      . ?
  */
 template <class C>
-struct has_Facade
-{
-    static constexpr auto _has_Facade()
-        -> decltype(typename C::Facade(), bool()) {
-        return true;
-    }
-    static constexpr bool value = _has_Facade();
-};
+struct has_Facade : std::integral_constant<
+    decltype(typename C::Facade(), bool()),
+    true>
+{};
+
 template <class C>
 struct is_Component
     : std::is_base_of<Component, C>
-    , std::enable_if<has_Facade<C>::value>
+    // , std::enable_if<has_Facade<C>::value>
 {};
-
 
 /** Provides unique identifiers for Components;
  *  Determines Component layout in entities.
@@ -42,30 +38,35 @@ template <class... Cpts>
 struct ComponentIndex
     : std::enable_if<util::all_satisfy<is_Component, Cpts...>::value>
 {
-    // struct ComponentVector
-    //     : util::TypeVector<std::add_pointer<Cpts>...>
-    // {};
-    // struct Facades
-    //     : util::TypeVector<std::add_pointer<typename Cpts::Facade>...>
-    // {};
-    
-    // template <class C> struct _facade_ptr
-    // { using type = typename std::add_pointer<typename C::Facade>; }
+    template <class C> struct _facade_ptr
+    { using type = typename std::add_pointer<typename C::Facade>; };
     
     using Components = typename
         util::TypeVector<std::add_pointer<Cpts>...>;
-    using Facades    = typename // no way to filter for Facade-defn's?
-        // util::filter_map<has_Facade, _facade_ptr, util::TypeVector, Cpts...>;
-        util::TypeVector<std::add_pointer<typename Cpts::Facade>...>;
-
+    using Facades    = typename
+        util::transform<_facade_ptr, typename
+                        util::filter<has_Facade,
+                                     util::TypeVector<Cpts...>
+                                     >::type>::type;
     
-    /** Type-checking helpers */
+    /* Type predicates */
+    /** Whether this ComponentIndex supports a given Component */
     template <class C>
     struct has_component : util::is_member<C, Cpts...> { };
-    /** Whether an entity implementing these components can be implemented */
+    /** Whether an Entity implementing these Components can be implemented */
     template <class... Cs>
     struct is_valid_entity : util::all_satisfy<has_component, Cs...> { };
            
 };
+
+#ifdef _BUILD_TEST
+
+struct C : Component { struct Facade {}; };
+
+void test() {
+    ComponentIndex<C> cix;
+    // static_assert(, "");
+}
+#endif
 
 #endif
