2e13a69ac51c70ee60e5820f4c9361d7964c1308
Roy Crihfield <rscrihf@gmail.com>
2013-09-03 19:36:04 -0500

    cleaned up functor interface

--- component_system.hh
+++ component_system.hh
@@ -1,3 +1,5 @@
+#include <utility>
+
 #include <boost/mpl/for_each.hpp>
 
 #include "component.hh"
@@ -6,8 +8,132 @@
 #ifndef _SCRATCH_COMPONENT_SYSTEM_
 #define _SCRATCH_COMPONENT_SYSTEM_
 
+
 namespace functor
 {
+    using boost::mpl::for_each;
+    
+    template <class, class... Args> auto make(Args&&...);
+    
+    template <class, class, class> struct InitComponent;
+    template <class> struct VerifyComponent;
+    template <class> struct UpdateSubsystem;
+}
+
+/**
+ * Represents a system which can handle the specified components.
+ * 
+ */
+template <class SystemIndex>
+struct ComponentSystem
+{
+    using Subsystems = typename util::transform<Subsystem, SystemIndex>;
+
+    struct Operation;
+    using OperationQueue = typename std::list<Operation>;
+
+
+    template <class> struct Entity;
+    // using Entities = std::vector
+
+
+    // members
+    Subsystems     subsystems ;
+    // Entities       entities   ;
+    OperationQueue op_queue   ;
+    
+    std::vector<bool> used_ids;
+
+    
+    void update()
+    {
+        using namespace functor;
+        auto f = make<UpdateSubsystem>(*this);
+        for_each<Subsystems>(f);
+    }
+    
+    void enqueue(Operation&& op) { }
+    
+    template <class EntityIndex, class Source>
+    Entity<EntityIndex>& create_entity(Source& src)
+    {
+        Entity<EntityIndex> entity;
+        EntityID id = fresh_id();
+
+        using namespace functor;
+
+        auto f = make<InitComponent>(*this, entity, src, id);
+        for_each<EntityIndex>(f);
+
+        return entity;
+    }
+
+    EntityID fresh_id()
+    {
+        EntityID id;
+
+        for (bool& used : used_ids) {
+            if (!used) {
+                used = true;
+                return id;
+            }
+            id++;
+        }
+        used_ids.push_back(true);
+        return id + 1;
+    }
+
+
+    /**** Operation ****
+     * performed by component
+     * on entities implementing component */
+    struct Operation
+    {
+        virtual void execute();
+    };
+
+
+    /**** Entity ****
+     *  Anything that "exists" within the system 
+     *  Contains a simple aggregation of components
+     */
+    template <class EntityIndex>
+    struct Entity
+    {
+        EntityIndex index;
+
+        template <class Cpt>
+        void add_component(Cpt* cpt)
+        {
+            using namespace functor;
+            
+            auto f = make<VerifyComponent>(*this);
+            for_each<Dependencies<Cpt> >(f);
+            
+            index.template get<Cpt>() = cpt;
+        }
+        
+    };
+};
+
+
+namespace functor
+{
+
+    // By C++14, this can be replaced with e.g.:
+    // for_each<EntityIndex>(
+    //     [&] <class Cpt> (Cpt _) {
+    //         auto sub = self.subsystems.template get<Cpt>();
+    //         entity.add_component(sub.create(src));
+    // ...
+    //     [&] <class Subsystem> (Subsystem _) {
+    //         subsystems.template get<Subsystem>().update();
+    //     });
+    template <class Functor, class... Args>
+    auto make(Args&&... args) {
+        return Functor(std::forward(args)...);
+    }
+    
     template <class ComponentSystem, class Entity, class Source>
     struct InitComponent
     {
@@ -22,7 +148,7 @@ namespace functor
             entity.add_component(sub.create(src));
         }
     };
-        
+
     template <class Entity>
     struct VerifyComponent
     {
@@ -50,94 +176,6 @@ namespace functor
     };
 
 }
-
-/**
- * Represents a system which can handle the specified components.
- * 
- */
-template <class SystemIndex>
-struct ComponentSystem
-{
-    using Subsystems = typename util::transform<Subsystem, SystemIndex>;
-
-    struct Operation;
-    using OperationQueue = typename std::list<Operation>;
-
-
-    template <class> struct Entity;
-    // using Entities = std::vector
-
-
-    // members
-    Subsystems     subsystems ;
-    // Entities       entities   ;
-    OperationQueue op_queue   ;
-
-    
-    void update()
-    {
-        functor::UpdateSubsystem<
-            decltype(*this)
-            > f{*this};
-
-        boost::mpl::for_each<Subsystems>(f);
-        // // with C++14:
-        // boost::mpl::for_each<Subsystems>(
-        //     [&] <class Subsystem> (Subsystem _) {
-        //         subsystems.template get<Subsystem>().update();
-        //     });
-    }
-    
-    void enqueue(Operation&& op) { }
-    
-    template <class EntityIndex, class Source>
-    Entity<EntityIndex>& create_entity(Source& src)
-    {
-        Entity<EntityIndex> entity;
-
-        functor::InitComponent<
-            decltype(*this),
-            Entity<EntityIndex>,
-            Source
-            > f{*this, entity, src};
-        
-        boost::mpl::for_each<EntityIndex>(f);
-    }
-
-
-    /**** Operation ****
-     * performed by component
-     * on entities implementing component */
-    struct Operation
-    {
-        virtual void execute();
-    };
-
-
-    /**** Entity ****
-     *  Anything that "exists" within the system 
-     *  Contains a simple aggregation of components
-     */
-    template <class EntityIndex>
-    struct Entity
-    {
-        EntityIndex index;
-
-        template <class Cpt>
-        void add_component(Cpt* cpt)
-        {
-            functor::VerifyComponent<
-                decltype(*this)
-                > f{*this};
-
-            boost::mpl::for_each<Dependencies<Cpt> >(f);
-            
-            index.template get<Cpt>() = cpt;
-        }
-        
-    };
-};
-
 
 // /** Create entity */
 // // does this need to be aware of a specific system?
