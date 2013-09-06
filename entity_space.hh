85de2637e00bf53aeab96b6fc722f9b286eed2a1
Roy Crihfield <rscrihf@gmail.com>
2013-09-06 10:21:26 -0500

    removed 'Subsystem'

--- entity_space.hh
+++ entity_space.hh
@@ -9,38 +9,6 @@
 #ifndef _SCRATCH_ENTITY_SPACE_
 #define _SCRATCH_ENTITY_SPACE_
 
-
-// Manages a single type of component
-template <class Component>
-struct Subsystem
-{
-    using Reference = Ref<Component>;
-    using Data = std::unordered_map<EntityID, Reference>;
-    Data data;
-
-    Subsystem()
-    {
-        data.reserve(100);
-    };
-
-
-    Reference create(EntityID ent_id) {
-        // assert(false && "Must implement specialized create() method");
-
-        // std::cout << "creating " << typeid(Component).name()
-        //           << " with ID = " << ent_id
-        //           << " at " << ref << "\n";
-
-        // data.emplace(ent_id, Component());
-        // return Reference {&data[ent_id]};
-        
-        Reference ref =
-            data[ent_id] = Reference {new Component()};
-
-        return ref;
-    }
-};
-
 /**
  * An environment which manages the specified components.
  * 
@@ -48,33 +16,17 @@ struct Subsystem
 template <class... Components>
 struct EntitySpace
 {
-    // using Subsystems = util::TypeVector<Subsystem<Components>...>;
     using Index = util::TypeVector<Components...>;
     using EntityType = Entity<Components...>;
     using Entities = std::vector<EntityType>;
     
     // members
-    // Subsystems m_subsystems;
     Entities m_entities;
-    
-
-    // struct UpdateSubsystem
-    // {
-    //     EntitySpace& self;
-
-    //     template <class Subsystem>
-    //     void operator()(Subsystem)
-    //     { self.template get_subsystem<Subsystem>().update(); }
-    // };
     
     void update()
     {
         
-        // util::expand_apply<Subsystems>(
-        //     util::make<UpdateSubsystem>(*this)
-        // );
     }
-    
     
     template <class... InitCpts>
     EntityID create_entity()
@@ -101,12 +53,8 @@ struct EntitySpace
     {
         return m_entities[id];
     }
-    
-    // template <class Cpt>
-    // Subsystem<Cpt>& get_subsystem()
-    // { return util::get<Subsystem<Cpt> >(m_subsystems); }
-    
 
+    
     /**** Operation ****
      * performed by component
      * on entities implementing component */
