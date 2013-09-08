1e5787a2dd89635de5c760b528300bca9c22228b
Roy Crihfield <rscrihf@gmail.com>
2013-09-08 00:08:04 -0500

    cleanup

--- component.hh
+++ component.hh
@@ -17,7 +17,6 @@
 using namespace util::io;
 #endif
 
-
 struct BasicComponent
 {
     std::string to_string() const
@@ -26,49 +25,11 @@ struct BasicComponent
     }
 };
 
-// CREATE_MEMBER_TEST(name);
-
-// template <class C>
-// util::enable_if_t<!has__name<C>::value,
-//                   std::string> name()
-// { return typeid(C).name(); }
-
-// template <class C>
-// util::enable_if_t<has__name<C>::value,
-//                   std::string> name()
-// { return C::name(); }
-
-
 /** Provides unique identifiers for Components;
  *  Determines Component layout in entities.
  */
 template <class... Cpts>
 using ComponentIndex = util::TypeVector<Cpts...>;
-
-
-// template <class... Cpts>
-// struct ComponentIndex
-//     : public util::TypeVector<Cpts...>
-//     // : public util::TypeVector<typename std::add_pointer<Cpts>::type...>
-// {
-//     // using Components = util::TypeVector<Cpts...>;
-
-    
-//     /* Type predicates */
-//     /** Whether this ComponentIndex supports a given Component */
-//     template <class> struct supports;
-    
-//     template <class Cpt>
-//     struct supports
-//         : public util::is_member<Cpt, Cpts...> { };
-
-//     template <template <class...> class CIx,
-//               class... OtherCpts>
-//     // others must form subset
-//     struct supports<CIx<OtherCpts...>>
-//         : public util::all_satisfy<supports, OtherCpts...> { };
-           
-// };
 
 
 #ifdef _BUILD_TEST
