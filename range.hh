edb6807d30515850a25af164607d71c6d0d90e24
Roy Crihfield <rscrihf@gmail.com>
2013-09-13 00:32:45 -0500

    fix FilterRange (const params, improve iterator)

--- range.hh
+++ range.hh
@@ -1,75 +1,74 @@
 #include <iterator>
-#include <type_traits>
 
 #ifndef _SCRATCH_RANGE
 #define _SCRATCH_RANGE
 
 namespace range
 {
-template <class Predicate, class IterBase>
-struct FilterRange
-{
-    struct iterator
-        : std::iterator_traits<IterBase>
-        // : IterBase
+    template <class Predicate, class IterBase>
+    struct FilterRange
     {
-        using value_type      =
-            typename std::iterator_traits<IterBase>::value_type;
-        using reference       =
-            typename std::iterator_traits<IterBase>::reference;
-        using pointer         =
-            typename std::iterator_traits<IterBase>::pointer;
-        using difference_type =
-            typename std::iterator_traits<IterBase>::difference_type;
+        using BaseTraits = typename std::iterator_traits<IterBase>;
+
+        struct Iterator : std::iterator<
+            std::forward_iterator_tag,
+            typename BaseTraits::value_type,
+            typename BaseTraits::difference_type,
+            typename BaseTraits::pointer,
+            typename BaseTraits::reference>
+        {
+            Predicate m_pred;
+            IterBase m_base, m_end;
+            
+            Iterator(const Predicate& pred,
+                     const IterBase& it, const IterBase& end)
+                : m_pred(pred), m_base(it), m_end(end)
+            { satisfy_predicate(); }
+        
+            typename BaseTraits::reference
+            operator*() const { return *m_base; }
+
+            Iterator& operator++()
+            {
+                ++m_base;
+                satisfy_predicate();
+            }
+
+            bool operator==(const Iterator& that)
+            { return m_base == that.m_base; }
+
+            bool operator!=(const Iterator& that)
+            { return !(*this == that); }
+    
+            void satisfy_predicate()
+            {
+                while (m_base != m_end && !m_pred(*m_base))
+                    ++m_base;
+            }
+        };
+    
+        FilterRange(const Predicate& f, const IterBase& it, const IterBase& end)
+            : m_pred(f), m_iter(it), m_end(end)
+        { }
+
+        Iterator begin() const
+        { return Iterator(m_pred, m_iter, m_end); }
+        Iterator end() const
+        { return Iterator(m_pred, m_end, m_end); }
 
         Predicate m_pred;
-        IterBase m_base, m_end;
-        
-        iterator(Predicate pred, IterBase it, IterBase end)
-            : m_pred(pred), m_base(it), m_end(end)
-        { satisfy_predicate(); }
-        
-        reference operator*() const { return *m_base; }
-
-        FilterRange& operator++()
-        {
-            ++m_base;
-            satisfy_predicate();
-        }
-
-        bool operator==(iterator& that)
-        {
-            return m_base == that.m_base;
-        }
-
-        bool operator!=(iterator& that)
-        { return !(*this == that); }
-    
-        void satisfy_predicate()
-        {
-            while (m_base != m_end && !m_pred(*m_base))
-                ++m_base;
-        }
+        IterBase m_iter, m_end;
     };
+
+    template <class Predicate, class It>
+    FilterRange<Predicate, It>&&
+    filter(It it, It end, Predicate&& f)
+    {
+        return FilterRange<Predicate, It>(
+            std::forward<Predicate>(f),
+            std::forward<It>(it), std::forward<It>(end));
+    }
     
-    FilterRange(Predicate f, IterBase it, IterBase end)
-        : m_pred(f), m_iter(it), m_end(end)
-    { }
-
-    iterator begin() const { return iterator(m_pred, m_iter, m_end); }
-    iterator end() const { return iterator(m_pred, m_end, m_end); }
-
-    Predicate m_pred;
-    IterBase m_iter, m_end;
-};
-
-template <class Predicate, class Iterator>
-FilterRange<Predicate, Iterator>&&
-filter(Iterator it, Iterator end, Predicate&& f)
-{
-    return FilterRange<Predicate, Iterator>(
-        std::forward<Predicate>(f), it, end);
-}
 }
 
 #endif
--- range_test.cc
+++ range_test.cc
@@ -8,12 +8,7 @@ int main()
     int nums[] = { 0, -1, 4, -3, 5, 8, -2 };
     const int N = sizeof(nums)/sizeof(int);
 
-    auto is_pos = [] (int x) { return 0 < x; };
-    auto filt = range::filter(is_pos, nums, nums + N);
-    // auto filt_end = filter(is_pos, nums + N, nums + N);
+    auto pos = range::filter(nums, nums + N, [] (int x) { return 0 < x; });
 
-    // for (auto it = filt.begin(); it != filt.end(); ++it)
-    for (auto x : filt)
-        util::print(x, ' ');
-    util::println();
+    util::println(pos);
 }
