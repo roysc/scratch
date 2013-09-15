54acd941e1d094a94a7708ef29704c13f6e621eb
Roy Crihfield <rscrihf@gmail.com>
2013-09-15 17:47:25 -0500

    cleanup filter.hh, add constraint

--- range/filter.hh
+++ range/filter.hh
@@ -3,11 +3,9 @@
 
 namespace range
 {
-    // template <class Predicate, class It>
     template <class Predicate, class R>
     struct FilterRange
     {
-        // using BaseTraits = std::iterator_traits<It>;
         using BaseTraits = typename R::BaseTraits;
         using ElementType = typename BaseTraits::value_type;
         using Reference = ElementType&;
@@ -19,36 +17,30 @@ namespace range
             satisfy_predicate();
         }
         
-        // Reference front() const { return *m_cursor; }
         Reference front() const { return m_input.front(); }
 
         void pop_front()
         {
-            // ++m_cursor;
             m_input.pop_front();
             satisfy_predicate();
         }
 
-        // bool has_next() const { return m_cursor != m_end; }
         bool has_next() const { return m_input.has_next(); }
 
       private:
         void satisfy_predicate()
         {
-            // while (m_cursor != m_end && !m_pred(*m_cursor))
-            //     ++m_cursor;
-
             while (m_input.has_next() && !m_pred(m_input.front()))
                 m_input.pop_front();
         }
         
       private:
         Predicate m_pred;
-        // It m_cursor, m_end;
         R m_input;
     };
 
-    template <class Predicate, class R>
+    template <class Predicate, class R,
+              class = std::enable_if_t<is_range<R>::value> >
     FilterRange<Predicate, R>
     filter(Predicate&& f, const R& r)
     {
@@ -60,7 +52,6 @@ namespace range
     filter(Predicate&& f, It it, It end)
     {
         return FilterRange<Predicate, AsRange<It> >(
-            // std::forward<Predicate>(f), it, end);
             std::forward<Predicate>(f), as_range(it, end));
     }
 
