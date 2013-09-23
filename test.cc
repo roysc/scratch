3bc187d9af20d2d91ae2536244f14942032c4066
Roy Crihfield <rscrihf@gmail.com>
2013-09-22 21:53:00 -0500

    add Mass & Energy components to test

--- test.cc
+++ test.cc
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <random>
+#include <cmath>
 
 #include <cassert>
 
@@ -10,9 +12,9 @@
 
 using namespace util::io;
 
-struct Vec2 : public BasicComponent
+struct Vec2// : public BasicComponent
 {
-    using Number = int;
+    using Number = float;
     using Array = std::array<Number, 2>;
     union {                                     
         Array v;
@@ -41,15 +43,33 @@ struct Vec2 : public BasicComponent
         using Vec2::Vec2;                                        \
     }
 
-EXTEND_Vec2(Position);
-EXTEND_Vec2(Velocity);
-EXTEND_Vec2(Acceleration);
+EXTEND_Vec2(Pos);
+EXTEND_Vec2(Vel);
+EXTEND_Vec2(Accel);
 
-struct Motion : public Logic<Position, Velocity>
+struct Energy// : public BasicComponent
 {
-    void operate(Position& p, Velocity& v)
+    float e;
+    std::string to_string() const
     {
-        // Logic<Position, Velocity>::operate(p, v);
+        return util::text("E(", e, ")");
+    }
+};
+
+struct Mass
+{
+    float m;
+    std::string to_string() const
+    {
+        return util::text("M(", m, ")");
+    }
+};
+
+struct Motion : public Logic<Pos, Vel>
+{
+    void operate(Pos& p, Vel& v)
+    {
+        // Logic<Pos, Vel>::operate(p, v);
             
         // println("Motion operating on (", p, ", ", v, ")");
         p.x += v.x;
@@ -57,13 +77,21 @@ struct Motion : public Logic<Position, Velocity>
     }
 };
 
-struct Motion2 : public Logic<Velocity, Acceleration>
+struct Motion2 : public Logic<Vel, Accel>
 {
-    void operate(Velocity& v, Acceleration& a)
+    void operate(Vel& v, Accel& a)
     {
         // println("Motion operating on (", p, ", ", v, ")");
         v.x += a.x;
         v.y += a.y;
+    }
+};
+
+struct KineticEnergy : public Logic<Vel, Mass, Energy>
+{
+    void operate(Vel& v, Mass& m, Energy& e)
+    {
+        e.e += sqrt(v.x * v.x + v.y * v.y) * m.m;
     }
 };
 
@@ -73,27 +101,36 @@ int main(int argc, char* argv[]) {
     int n_updates = argc > 1 ? std::stoi(argv[1]) : 10,
         n_entities = argc > 2 ? std::stoi(argv[2]) : 1;
 
-    using Space = EntitySpace<Position, Velocity, Acceleration>;
+    using Space = EntitySpace<Pos, Vel, Accel, Mass, Energy>;
     // Space space;
     
-    using System = System<Space, Motion2, Motion>;
-    System sys;
+    using System = System<Space, Motion2, Motion, KineticEnergy>;
 
+    std::default_random_engine rng;
+    std::uniform_int_distribution<int> di(-100, 100);
+    std::uniform_real_distribution<float> df(0, 1);
+    std::uniform_real_distribution<float> dfn(-1, 1);
+    
+    System sys;
     std::vector<EntityID> ents;
 
     for (int i = 0; i < n_entities; ++i) {
-        ents.push_back(sys.template create_entity<Position>());
-        ents.push_back(sys.create_entity(Position (1,1)));
-        ents.push_back(sys.template create_entity<Velocity>());
-        // va.push_back(sys.create_entity(Velocity()));
-        // pv.push_back(sys.template create_entity<Position, Velocity>());
+        // ents.push_back(sys.template create_entity<Pos>());
+        // ents.push_back(sys.create_entity(Pos (1,1)));
+        // ents.push_back(sys.template create_entity<Vel>());
+        // va.push_back(sys.create_entity(Vel()));
+        // pv.push_back(sys.template create_entity<Pos, Vel>());
+        // ents.push_back(sys.create_entity(
+        //                   Pos(),
+        //                   Vel(1,-1)));
+        //                   Vel(1,-1)));
         ents.push_back(sys.create_entity(
-                          Position(),
-                          Velocity(1,-1)));
-        ents.push_back(sys.create_entity(
-                          Position(),
-                          Velocity(),
-                          Acceleration(-1, 1)));
+                           Pos(),
+                           Vel(),
+                           Accel(dfn(rng), dfn(rng)),
+                           Energy {df(rng)},
+                           Mass {df(rng)}
+                           ));
     }
     
     // for (auto ent : v) println(sys.space[ent]);
@@ -110,7 +147,7 @@ int main(int argc, char* argv[]) {
 
     // game.enqueue(
     //     CSpace::Spawn<RandomInput>(1),
-    //     CSpace::Spawn<Position>(n_ent, )
+    //     CSpace::Spawn<Pos>(n_ent, )
     // );
 
     // TextUI ui;
